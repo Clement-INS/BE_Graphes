327d662f139cc2184e74eb2bb81f5c719a3a4dc4
Vigand Clement <vigand@insa-20036>
2021-03-31 18:20:27 +0200

    debut dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,9 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.model.Label;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,7 +14,21 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        Graph graph = data.getGraph();
+        BinaryHeap<Integer> BH = new BinaryHeap<Integer>();
+        // initialization
+        for (int i = 0; i < graph.size(); i++) {
+        	Label l;
+        	if (graph.getNodes().get(i).equals(data.getOrigin())) {
+        		l = new Label(i, true);
+        		BH.insert(i);
+        	}
+        	else {
+        		l = new Label(i, false);
+        	}
+        	Label.Labels.add(l);
+        }
         return solution;
     }
 
